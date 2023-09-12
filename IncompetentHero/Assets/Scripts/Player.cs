449d73fb605d30c30171f4e4907ac3abbc99df48
KHS <kij4300@naver.com>
2023-09-12 18:02:27 +0900

    취소
    메인으로 잘못 들어 온 거 취소

---
+++ IncompetentHero/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    //private Vector2 moveDirection = Vector2.zero;
+
+    private Vector2 moveVector = Vector2.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    private void GetInput()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+
+        //Debug.Log("moveVector: " + moveVector);
+    }
+
+    private void Move()
+    {
+        transform.Translate(moveVector * speed * Time.deltaTime);
+
+        ConstrainToCameraBounds();
+    }
+
+    /// <summary>
+    /// 카메라 경계 밖으로 플레이어가 못 나가게 한다.
+    /// </summary>
+    private void ConstrainToCameraBounds()
+    {
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (viewPos.x < 0f)
+        {
+            viewPos.x = 0f;
+        }
+
+        if (viewPos.x > 1f)
+        {
+            viewPos.x = 1f;
+        }
+
+        transform.position = Camera.main.ViewportToWorldPoint(viewPos);
+    }
+}
