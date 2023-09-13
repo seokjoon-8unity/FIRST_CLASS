d17a59b9b34f7219f105fb933fa4a26811d5ac4a
diamondgoelem <kimjong82355@naver.com>
2023-09-13 17:12:30 +0900

    버튼
    버튼 기능 수정

--- IncompetentHero/Assets/Scripts/DeadImageMainButton.cs
+++ IncompetentHero/Assets/Scripts/DeadImageMainButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class DeadImageMainButton : MonoBehaviour
 {
     public void Change()
     {
-        SceneManager.LoadScene("Intro");
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
